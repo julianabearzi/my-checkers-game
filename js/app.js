b6b79bd42ff1b6e79454b144a4cf4d170cb1baa3
Juliana Bearzi <julietad299@gmail.com>
2021-06-01 09:07:51 -0300

    i tried to show a static started checkers game

--- js/app.js
+++ js/app.js
@@ -10,18 +10,24 @@ var board = [
     [null, null, null, null, null, null, null, null]
 ];
 var turn = 0;
+var brownScore = 0;
+var blackScore = 0;
 
 
 function TurnPiece() {
-    
+    var score = 0;
     if (turn === 1) {
-      document.getElementById('turn').classList.add('p1'); 
+      document.getElementById('turn').classList.add('p1');
+      score++;
+      brownScore.innerHTML = 'Score: ' + score; 
     } else {
       document.getElementById('turn').classList.add('p2');
+      score++;
+      blackScore.innerHTML = 'Score: ' + score;
     }
   }
 
-var renderBoard = function () {
+/*var renderBoard = function () {
     var boardMatrix = '';
     for (var x = 0; x < board.length; x++) {
         boardMatrix += '<tr id="' + x + '" class="row">'
@@ -41,7 +47,47 @@ var renderBoard = function () {
     }
    checkers.innerHTML = boardMatrix;
    TurnPiece(turn);
-   
+}*/
+var renderBoard = function () {
+    var boardMatrix = '';
+    if(turn === 1) {
+    for (var x = 0; x < board.length; x++) {
+        boardMatrix += '<tr id="' + x + '" class="row">'
+        for (var y = board[x].length - 1; y >= 0; y--) {
+            boardMatrix += '<td id="' + x + y + '" class="cell'
+           if ((x === 0 &&  y % 2 === 0) || (x === 1 && y % 2 === 1) || (x === 2 && y % 2 === 0 && y != 4) || (x === board.length - 3 && y === 1)) {
+            boardMatrix +='"><img src="./img/black.png" class="checkers__piece--black" alt="black"></td>';
+           }
+           else if ((x === board.length - 2 && y % 2 === 0) || (x === board.length - 1 && y % 2 === 1) || (x === board.length - 3 && y % 2 === 1) ) {
+            boardMatrix += '"><img src="./img/brown.png" class="checkers__piece--brown" alt="brown"></td>';
+           }
+           else {
+            boardMatrix += '"></td>';
+           }         
+        }
+        boardMatrix += '</tr>';
+    }
+    }
+    else {
+        for (var x = 0; x < board.length; x++) {
+            boardMatrix += '<tr id="' + x + '" class="row">'
+            for (var y = board[x].length - 1; y >= 0; y--) {
+                boardMatrix += '<td id="' + x + y + '" class="cell'
+                if ((x === board.length - 2 && y % 2 === 0) || (x === board.length - 1 && y % 2 === 1) || (x === board.length - 3 && y % 2 === 1 && y != 1) || (x === 2 && y === 4)){
+                    boardMatrix += '"><img src="./img/brown.png" class="checkers__piece--brown" alt="brown"></td>';
+                   }
+               else if ((x === 0 &&  y % 2 === 0) || (x === 1 && y % 2 === 1) || (x === 2 && y % 2 === 0)) {
+                boardMatrix +='"><img src="./img/black.png" class="checkers__piece--black" alt="black"></td>';
+               }
+               else {
+                boardMatrix += '"></td>';
+               }         
+            }
+            boardMatrix += '</tr>';
+        }
+    }
+   checkers.innerHTML = boardMatrix;
+   TurnPiece(turn); 
 }
 
 
@@ -50,6 +96,12 @@ var init = function () {
     row = document.getElementsByClassName('row');
     blackPieces = document.querySelectorAll('checkers__piece--black');
     brownPieces = document.querySelectorAll('checkers__piece--brown');
+    brownScore = document.getElementById('player__score');
+    blackScore = document.getElementById('player__score--two');
+    player1 = document.getElementById('player__name');
+    turn = Math.random() > 0.5 ? 1 : 2
+
+
     renderBoard(); 
 }
 window.onload = init;
